4b6cacfbf8fbbb760c8c8abeadcb57ec843e5ddb
Douglas L. Tucker <dtucker@fnal.gov>
2017-03-07 17:44:41 -0600

    Addeded setup scripts

---
+++ bin/setup.bash
@@ -0,0 +1,10 @@
+# You will want to set up a Python environment that contains
+#  the Python numpy and scipy modules.
+
+# Also, you will want to edit the PYEXCAL_DIR below to point to
+#  your pyExcal direcotory and source this script (if you are 
+#  running bash) or the equivalent setup.csh script (if you are
+#  running tcsh)...
+export PYEXCAL_DIR=/Users/dtucker/GitHub/pyExcal
+export PATH=${PYEXCAL_DIR}/bin:${PATH}
+export PYTHONPATH=${PYEXCAL_DIR}/python:${PYTHONPATH}
---
+++ bin/setup.csh
@@ -0,0 +1,10 @@
+# You will want to set up a Python environment that contains
+#  the Python numpy and scipy modules.
+
+# Also, you will want to edit the PYEXCAL_DIR below to point to
+#  your pyExcal direcotory and source this script (if you are 
+#  running tsh) or the equivalent setup.bash script (if you are
+#  running bash)...
+setenv PYEXCAL_DIR /Users/dtucker/GitHub/pyExcal
+setenv PATH ${PYEXCAL_DIR}/bin:$PATH
+setenv PYTHONPATH ${PYEXCAL_DIR}/python:$PYTHONPATH
