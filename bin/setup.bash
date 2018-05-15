52e442aaec8b13fa92c6cc5f5884601aad02a946
Douglas L. Tucker <dtucker@fnal.gov>
2018-05-15 17:45:10 -0500

    Updated setup.bash and setup.csh to contain STILTS_DIR

--- bin/setup.bash
+++ bin/setup.bash
@@ -1,10 +1,13 @@
 # You will want to set up a Python environment that contains
-#  the Python numpy and scipy modules.
+#  the Python numpy, scipy, and astropy  modules.
 
 # Also, you will want to edit the PYEXCAL_DIR below to point to
-#  your pyExcal direcotory and source this script (if you are 
-#  running bash) or the equivalent setup.csh script (if you are
-#  running tcsh)...
+#  your pyExcal directory, and edit the STILTS_DIR to point to 
+#  your stilts directory,  and then source this script (if you 
+#  are running bash) or the equivalent setup.csh script (if you 
+#  are running tcsh)...
 export PYEXCAL_DIR=/Users/dtucker/GitHub/pyExcal
 export PATH=${PYEXCAL_DIR}/bin:${PATH}
 export PYTHONPATH=${PYEXCAL_DIR}/python:${PYTHONPATH}
+
+export STILTS_DIR=/Users/dtucker/Software/STILTS/latest/stilts
--- bin/setup.csh
+++ bin/setup.csh
@@ -1,10 +1,13 @@
 # You will want to set up a Python environment that contains
-#  the Python numpy and scipy modules.
+#  the Python numpy, scipy, and astropy  modules.
 
 # Also, you will want to edit the PYEXCAL_DIR below to point to
-#  your pyExcal direcotory and source this script (if you are 
-#  running tsh) or the equivalent setup.bash script (if you are
-#  running bash)...
+#  your pyExcal directory, and edit the STILTS_DIR to point to 
+#  your stilts directory,  and then source this script (if you 
+#  are running tcsh) or the equivalent setup.sh script (if you 
+#  are running bash)...
 setenv PYEXCAL_DIR /Users/dtucker/GitHub/pyExcal
 setenv PATH ${PYEXCAL_DIR}/bin:$PATH
 setenv PYTHONPATH ${PYEXCAL_DIR}/python:$PYTHONPATH
+
+setenv STILTS_DIR /Users/dtucker/Software/STILTS/latest/stilts
